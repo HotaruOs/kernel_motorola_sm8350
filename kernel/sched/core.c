7655948bb96c666be11a062047a89384e66d1698
Peter Zijlstra <peterz@infradead.org>
2022-03-16 22:03:41 +0100

    sched/core: Fix forceidle balancing
    Steve reported that ChromeOS encounters the forceidle balancer being
    ran from rt_mutex_setprio()'s balance_callback() invocation and
    explodes.
    
    Now, the forceidle balancer gets queued every time the idle task gets
    selected, set_next_task(), which is strictly too often.
    rt_mutex_setprio() also uses set_next_task() in the 'change' pattern:
    
    	queued = task_on_rq_queued(p); /* p->on_rq == TASK_ON_RQ_QUEUED */
    	running = task_current(rq, p); /* rq->curr == p */
    
    	if (queued)
    		dequeue_task(...);
    	if (running)
    		put_prev_task(...);
    
    	/* change task properties */
    
    	if (queued)
    		enqueue_task(...);
    	if (running)
    		set_next_task(...);
    
    However, rt_mutex_setprio() will explicitly not run this pattern on
    the idle task (since priority boosting the idle task is quite insane).
    Most other 'change' pattern users are pidhash based and would also not
    apply to idle.
    
    Also, the change pattern doesn't contain a __balance_callback()
    invocation and hence we could have an out-of-band balance-callback,
    which *should* trigger the WARN in rq_pin_lock() (which guards against
    this exact anti-pattern).
    
    So while none of that explains how this happens, it does indicate that
    having it in set_next_task() might not be the most robust option.
    
    Instead, explicitly queue the forceidle balancer from pick_next_task()
    when it does indeed result in forceidle selection. Having it here,
    ensures it can only be triggered under the __schedule() rq->lock
    instance, and hence must be ran from that context.
    
    This also happens to clean up the code a little, so win-win.
    
    Fixes: d2dfa17bc7de ("sched: Trivial forced-newidle balancer")
    Reported-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: T.J. Alumbaugh <talumbau@chromium.org>
    Link: https://lkml.kernel.org/r/20220330160535.GN8939@worktop.programming.kicks-ass.net
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5290,6 +5290,8 @@ static inline struct task_struct *pick_task(struct rq *rq)
 
 extern void task_vruntime_update(struct rq *rq, struct task_struct *p, bool in_fi);
 
+static void queue_core_balance(struct rq *rq);
+
 static struct task_struct *
 pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 {
@@ -5339,7 +5341,7 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		}
 
 		rq->core_pick = NULL;
-		return next;
+		goto out;
 	}
 
 	put_prev_task_balance(rq, prev, rf);
@@ -5389,7 +5391,7 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 			 */
 			WARN_ON_ONCE(fi_before);
 			task_vruntime_update(rq, next, false);
-			goto done;
+			goto out_set_next;
 		}
 	}
 
@@ -5508,8 +5510,12 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		resched_curr(rq_i);
 	}
 
-done:
+out_set_next:
 	set_next_task(rq, next);
+out:
+	if (rq->core->core_forceidle_count && next == rq->idle)
+		queue_core_balance(rq);
+
 	return next;
 }
 
@@ -5604,7 +5610,7 @@ static void sched_core_balance(struct rq *rq)
 
 static DEFINE_PER_CPU(struct callback_head, core_balance_head);
 
-void queue_core_balance(struct rq *rq)
+static void queue_core_balance(struct rq *rq)
 {
 	if (!sched_core_enabled(rq))
 		return;
