fd3b71c825c30b968c6694f646aef210fed1f57c
skye-pa1n <sonicgames772@gmail.com>
2025-01-01 16:58:21 -0500

    introduce adreno gpu boost

--- drivers/gpu/msm/governor_msm_adreno_tz.c
+++ drivers/gpu/msm/governor_msm_adreno_tz.c
@@ -19,6 +19,7 @@
 
 #include "../../devfreq/governor.h"
 #include "msm_adreno_devfreq.h"
+#include <linux/of_platform.h>
 
 static DEFINE_SPINLOCK(tz_lock);
 static DEFINE_SPINLOCK(sample_lock);
@@ -54,6 +55,10 @@ static DEFINE_SPINLOCK(suspend_lock);
 
 #define TAG "msm_adreno_tz: "
 
+#if 1
+static unsigned int adrenoboost = 1;
+#endif
+
 static u64 suspend_time;
 static u64 suspend_start;
 static unsigned long acc_total, acc_relative_busy;
@@ -83,6 +88,31 @@ u64 suspend_time_ms(void)
 	suspend_start = suspend_sampling_time;
 	return time_diff;
 }
+
+#if 1
+static ssize_t adrenoboost_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	size_t count = 0;
+	count += sprintf(buf, "%d\n", adrenoboost);
+
+	return count;
+}
+
+static ssize_t adrenoboost_save(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	int input;
+	sscanf(buf, "%d ", &input);
+	if (input < 0 || input > 3) {
+		adrenoboost = 0;
+	} else {
+		adrenoboost = input;
+	}
+
+	return count;
+}
+#endif
 
 static ssize_t gpu_load_show(struct device *dev,
 		struct device_attribute *attr,
@@ -159,6 +189,11 @@ static ssize_t mod_percent_show(struct device *dev,
 
 static DEVICE_ATTR_RO(gpu_load);
 
+#if 1
+static DEVICE_ATTR(adrenoboost, 0644,
+		adrenoboost_show, adrenoboost_save);
+#endif
+
 static DEVICE_ATTR_RO(suspend_time);
 static DEVICE_ATTR_RW(mod_percent);
 
@@ -166,6 +201,10 @@ static const struct device_attribute *adreno_tz_attr_list[] = {
 		&dev_attr_gpu_load,
 		&dev_attr_suspend_time,
 		&dev_attr_mod_percent,
+
+#if 1
+		&dev_attr_adrenoboost,
+#endif
 		NULL
 };
 
@@ -376,6 +415,16 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 	stats->busy_time = min_t(u64, busy_time, stats->total_time);
 
 	priv->bin.busy_time += stats->busy_time;
+#if 1
+	// scale busy time up based on adrenoboost parameter, only if MIN_BUSY exceeded...
+	if ((unsigned int)(priv->bin.busy_time + stats->busy_time) >= MIN_BUSY) {
+		priv->bin.busy_time += stats->busy_time * (1 + (adrenoboost*3)/2);
+	} else {
+		priv->bin.busy_time += stats->busy_time;
+	}
+#else
+	priv->bin.busy_time += stats.busy_time;
+#endif
 
 	if (stats->private_data)
 		context_count =  *((int *)stats->private_data);
@@ -547,12 +596,15 @@ static int tz_handler(struct devfreq *devfreq, unsigned int event, void *data)
 	struct msm_adreno_extended_profile *gpu_profile;
 	struct device_node *node = devfreq->dev.parent->of_node;
 
+	/*
+	 * We want to restrict this governor be set only for
+	 * gpu devfreq devices.
+	 */
 	if (!of_device_is_compatible(node, "qcom,kgsl-3d0"))
 		return -EINVAL;
 
 	gpu_profile = container_of((devfreq->profile),
-			struct msm_adreno_extended_profile,
-			profile);
+		struct msm_adreno_extended_profile, profile);
 
 	switch (event) {
 	case DEVFREQ_GOV_START:
