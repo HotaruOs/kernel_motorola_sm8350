a5cc453427ee9bbeaf04813e68d4493fb624c2d2
TogoFire <togofire@mailfence.com>
2025-01-30 22:09:51 -0300

    Merge 5.4.289 into HEAD
    ref:
    https://android.googlesource.com/kernel/common.git/+/refs/heads/android11-5.4-lts
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: 8bab80b4af8d50fa8dc5082df566fe685adb2a63

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -1669,8 +1669,9 @@ static int __qcom_glink_send(struct glink_channel *channel,
 	ret = qcom_glink_tx(glink, &req, sizeof(req), data, chunk_size, wait);
 
 	/* Mark intent available if we failed */
-	if (ret && intent) {
-		intent->in_use = false;
+	if (ret) {
+		if (intent)
+			intent->in_use = false;
 		return ret;
 	}
 
@@ -1691,8 +1692,9 @@ static int __qcom_glink_send(struct glink_channel *channel,
 				    chunk_size, wait);
 
 		/* Mark intent available if we failed */
-		if (ret && intent) {
-			intent->in_use = false;
+		if (ret) {
+			if (intent)
+				intent->in_use = false;
 			break;
 		}
 	}
