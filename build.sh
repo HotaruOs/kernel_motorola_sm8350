074cca6af4d4a146f6b78216209a1a4ba8b823c0
HotaruOs <arroztlg@gmail.com>
2025-07-24 23:05:53 -0300

    add initial kernel build script

---
+++ build.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+kernel_dir="${PWD}"
+CCACHE=$(command -v ccache)
+objdir="${kernel_dir}/out"
+anykernel=$HOME/anykernel
+builddir="${kernel_dir}/build"
+ZIMAGE=$kernel_dir/out/arch/arm64/boot/Image
+kernel_name="la-pão"
+zip_name="$kernel_name-$(date +"%d%m%Y-%H%M").zip"
+TC_DIR="${PWD}/tc"
+export CONFIG_FILE3="arch/arm64/configs/vendor/lineage_tundra.config"
+export CONFIG_FILE2="arch/arm64/configs/vendor/lineage_moto-lahaina.config"
+export CONFIG_FILE="vendor/lahaina-qgki_defconfig"
+export ARCH="arm64"
+export KBUILD_BUILD_HOST=Pão
+export KBUILD_BUILD_USER=HotaruOs
+
+export PATH="$TC_DIR/bin:$PATH"
+if ! [ -d "$TC_DIR" ]; then
+	echo "AOSP clang not found! Cloning to $TC_DIR..."
+	if ! git clone --depth=1 -b 18 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone "$TC_DIR"; then
+		echo "Cloning failed! Aborting..."
+		exit 1
+	fi
+fi
+
+# Colors
+NC='\033[0m'
+RED='\033[0;31m'
+LRD='\033[1;31m'
+LGR='\033[1;32m'
+
+export PATH="$TC_DIR/bin:$PATH"
+echo -e "${LGR}######### Versão do Clang #########${NC}"
+$TC_DIR/bin/clang --version
+
+make_defconfig() {
+    START=$(date +"%s")
+    echo -e "${LGR}########### Generating defconfig ############${NC}"
+
+    mkdir -p "${objdir}"
+
+    make -s ARCH=${ARCH} O=${objdir} CC=clang HOSTCC=clang ${CONFIG_FILE} LLVM=1 LLVM_IAS=1 -j$(nproc --all)
+
+    echo -e "${LGR}########### Merging Additional Configs ############${NC}"
+    scripts/kconfig/merge_config.sh -O "${objdir}" "${objdir}/.config" ${CONFIG_FILE2} ${CONFIG_FILE3} 
+
+    make -s ARCH=${ARCH} O=${objdir} CC=clang HOSTCC=clang olddefconfig LLVM=1 LLVM_IAS=1 -j$(nproc --all)
+
+    END=$(date +"%s")
+    echo -e "${LGR}########### Defconfig generated in  $((END - START))s ############${NC}"
+}
+
+compile() {
+    cd ${kernel_dir}
+    echo -e ${LGR} "######### Compiling kernel #########${NC}"
+    make -j$(nproc --all) \
+    O=out \
+    ARCH=${ARCH}\
+    CC="ccache clang" \
+    CLANG_TRIPLE="aarch64-linux-gnu-" \
+    CROSS_COMPILE="aarch64-linux-gnu-" \
+    CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+    LLVM=1 \
+    LLVM_IAS=1
+}
+
+completion() {
+    cd ${objdir}
+    COMPILED_IMAGE=arch/arm64/boot/Image
+    DTB_DIR=arch/arm64/boot/dts/vendor/qcom/
+    COMPILED_DTBO=arch/arm64/boot/dts/vendor/qcom/*.img
+    COMPILED_DTB=arch/arm64/boot/dts/vendor/qcom/lahaina*dtb
+    if [[ -f ${COMPILED_IMAGE} ]]; then
+
+        git clone -q https://github.com/HotaruOs/AnyKernel3 -b HoS $anykernel
+
+        cp -f ${COMPILED_IMAGE} $anykernel
+        cp -f "${DTB_DIR}"/*.img $anykernel
+        mkdir -p $anykernel/dtb
+        cp -f "${DTB_DIR}"/lahaina*dtb $anykernel/dtb
+        cd $anykernel
+        find . -name "*.zip" -type f
+        find . -name "*.zip" -type f -delete
+        zip -r AnyKernel.zip *
+        cp AnyKernel.zip $zip_name
+        cp $anykernel/$zip_name $kernel_dir/$zip_name
+        rm -rf $anykernel
+        END=$(date +"%s")
+        DIFF=$(($END - $START))
+        curl -F "file=@$kernel_dir/$zip_name" https://temp.sh/upload
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+        echo -e ${LGR} "############################################"
+        echo -e ${LGR} "########### Kernel compiled successfully ############"
+        echo -e ${LGR} "############################################${NC}"
+    else
+        echo -e ${RED} "############################################"
+        echo -e ${RED} "############ Failed to compile Kernel #############"
+        echo -e ${RED} "############################################${NC}"
+    fi
+}
+
+make_defconfig
+compile
+completion
+cd ${kernel_dir}
